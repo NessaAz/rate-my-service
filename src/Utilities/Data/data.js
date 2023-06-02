eebdb9fc6423da06f2288a47d6a8fb66dab79558
Vincent <makokhavomondi@gmail.com>
2023-06-02 20:15:18 +0300

    Update default data positions on canvas

--- src/Utilities/Data/data.js
+++ src/Utilities/Data/data.js
@@ -2,7 +2,7 @@ const data = [
     {
         id: 1,
         type: "text",
-        size: {width: "20%", height: "16%"},
+        size: {width: "50%", height: "10%"},
         position: {top: "20%", left: "10%"},
         element: "p",
         content: "The quick brown fox jumps over the lazy white dog",
@@ -13,8 +13,8 @@ const data = [
     {
         id: 2,
         type: "text",
-        size: {width: "20%", height: "16%"},
-        position: {top: "50%", left: "60%"},
+        size: {width: "50%", height: "10%"},
+        position: {top: "50%", left: "40%"},
         element: "p",
         content: "The quick brown fox jumps over the lazy white dog",
         contentStyle: {
