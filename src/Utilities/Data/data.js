078b240fd83ab4a295831d825ea9722a9068244f
Vincent <makokhavomondi@gmail.com>
2023-06-02 15:01:16 +0300

    Create data utility; we'll be using this data for now

---
+++ src/Utilities/Data/data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        id: 1,
+        type: "text",
+        content: "The quick brown fox jumps over the lazy white dog"
+    },
+    {
+        id: 2,
+        type: "text",
+        content: "The quick brown fox jumps over the lazy white dog"
+    }
+]
+
+export default data;
