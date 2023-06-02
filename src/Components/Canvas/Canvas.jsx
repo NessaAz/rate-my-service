66640881d3a952af41d3cfa87538d9d4daf227c3
Vincent <makokhavomondi@gmail.com>
2023-06-02 10:21:12 +0300

    Add initial structure - components, elements, pages, etc.

---
+++ src/Components/Canvas/Canvas.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Canvas.css"
+
+function Canvas(){
+    return (
+        <></>
+    )
+}
+
+export default Canvas;
---
+++ src/Components/Text/Text.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Text.css"
+
+function Text(){
+    return (
+        <></>
+    )
+}
+
+export default Text;
