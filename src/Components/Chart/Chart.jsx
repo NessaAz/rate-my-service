d5c260ce77689dd88aed7c14d253036fa6f5df32
Vincent <makokhavomondi@gmail.com>
2023-06-03 10:54:50 +0300

    Fix a bug with chart height not being modifiable by user activity

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -4,17 +4,16 @@ import createChart from "../../Utilities/Highcharts/CreateChart";
 
 function Chart({canvasObject, onClick}){
     const figureRef = useRef()
+    const itemStyle = {...canvasObject.size, ...canvasObject.position}
 
     useEffect(()=>{
         createChart(`${figureRef.current.id}`, canvasObject)
     }, [])
 
     return (
-        <div className="resize-drag yes-drop" onClick={()=>onClick(canvasObject)}>
-            <figure class="highcharts-figure">
-                <div ref={figureRef} id={`chart-container-${canvasObject.id}`}></div>
-                <p className="highcharts-description"></p>
-            </figure>
+        <div className="resize-drag yes-drop" onClick={()=>onClick(canvasObject)} style={itemStyle}>
+            <div className="chart-item" ref={figureRef} id={`chart-container-${canvasObject.id}`}></div>
+            <p className="highcharts-description"></p>
         </div>
     )
 }
