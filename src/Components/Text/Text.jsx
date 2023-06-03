22537ff915302f3a32f875ed33f0ee215bb27905
Vincent <makokhavomondi@gmail.com>
2023-06-03 10:58:33 +0300

    Update text object
    Make it use default property from the canvasObject the component receives. Also improve its behavior on dragging and dropping

--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -5,8 +5,8 @@ function Text({canvasObject, onClick}){
     const itemStyle = {...canvasObject.size, ...canvasObject.position}
 
     return (
-        <div className="resize-drag yes-drop text-item" style={itemStyle} onClick={()=>onClick(canvasObject)}>
-            <p style={canvasObject.contentStyle}>{canvasObject.content}</p>
+        <div className="resize-drag yes-drop" style={itemStyle} onClick={()=>onClick(canvasObject)}>
+            <p className="text-item" style={canvasObject.contentStyle}>{canvasObject.content}</p>
         </div>
     )
 }
