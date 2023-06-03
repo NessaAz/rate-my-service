ae80a5d61fdbad3b5bad461b97785cd2e1714ca0
Vincent <makokhavomondi@gmail.com>
2023-06-03 22:49:40 +0300

    Add canvas object editor

---
+++ src/Pages/Presentation/Components/ObjectEditor/ObjectEditor.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ObjectEditor.css"
+
+function ObjectEditor(){
+    return (
+        <div id="object-editor"></div>
+    )
+}
+
+export default ObjectEditor;
--- src/Pages/Presentation/Presentation.jsx
+++ src/Pages/Presentation/Presentation.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./Presentation.css"
 import Canvas from "../../Components/Canvas/Canvas";
 import ObjectActions from "./Components/ObjectActions/ObjectActions";
+import ObjectEditor from "./Components/ObjectEditor/ObjectEditor";
 
 function Presentation({globalState, setGlobalState}){
     return (
         <div id="presentation-page">
-            <ObjectActions globalState={globalState} />
-            <Canvas globalState={globalState} setGlobalState={setGlobalState} />
+            <ObjectEditor />
+            <div id="presentation-container">
+                <ObjectActions globalState={globalState} />
+                <Canvas globalState={globalState} setGlobalState={setGlobalState} />
+            </div>
         </div>
     )
 }
