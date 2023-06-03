894cf411535ba0b5cefafb6840a5a90e778e7c71
Vincent <makokhavomondi@gmail.com>
2023-06-03 11:56:46 +0300

    Add navbar to presentation page

--- src/Pages/Presentation/Presentation.jsx
+++ src/Pages/Presentation/Presentation.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Canvas from "../../Components/Canvas/Canvas";
 import "./Presentation.css"
+import Navbar from "../../Components/Navbar/Navbar";
 
 function Presentation({globalState, setGlobalState}){
     return (
         <div id="presentation-page">
+            <Navbar />
             <Canvas globalState={globalState} setGlobalState={setGlobalState} />
         </div>
     )
