b4eade9d11ba1800ffa85b7f5fbccc8a95f7ca3c
Vincent <makokhavomondi@gmail.com>
2023-06-03 17:31:28 +0300

    Use css selector to create camelCased variable names when getting their sizes

--- src/Utilities/Functions/utilityFunctions.js
+++ src/Utilities/Functions/utilityFunctions.js
@@ -2,12 +2,30 @@ function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
+// Takes in a string such as "high-charts" and returns "highCharts"
+function makeCamelCase(string){
+    string = string.replace("_", " ") // because "_" is considered a word in regex
+    string = string.toLowerCase()
+    let stringArray = string.replace(/[^\w]/g, " ").split(" ").filter(s => s !== '')
+    stringArray = stringArray.map((strItem, index) => {
+        if(index == 0){
+            return strItem
+        }else {
+            return strItem.charAt(0).toUpperCase() + strItem.slice(1)
+        }
+    })
+
+    return stringArray.join("")
+}
+
 function getDOMElementSize(cssSelector){
     const element = document.querySelector(cssSelector)
+
+    const elementName = makeCamelCase(cssSelector)
     
     return {
-        height: element.offsetHeight,
-        width: element.offsetWidth
+        [`${elementName}Height`]: element.offsetHeight,
+        [`${elementName}Width`]: element.offsetWidth
     }
 }
 
