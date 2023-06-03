10447b8de0c01a6ed7d76a576ef063d510fc3e77
Vincent <makokhavomondi@gmail.com>
2023-06-03 19:36:02 +0300

    Show active item using a dashed border

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -4,14 +4,25 @@ import createChart from "../../Utilities/Highcharts/CreateChart";
 
 function Chart({canvasObject, onClick}){
     const figureRef = useRef()
+    const resizableDraggableRef = useRef()
     const itemStyle = {...canvasObject.size, ...canvasObject.position, transform: `scale(${canvasObject.scale})`}
 
     useEffect(()=>{
         createChart(`${figureRef.current.id}`, canvasObject)
     }, [])
 
+    function handleClick(canvasObject){
+        const resizableDraggableElements = document.querySelectorAll(".resize-drag")
+        resizableDraggableElements.forEach(element => {
+            element.classList.remove("active")
+        })
+
+        resizableDraggableRef.current.classList.add("active")
+        onClick(canvasObject)
+    }
+
     return (
-        <div className="resize-drag yes-drop" onClick={()=>onClick(canvasObject)} style={itemStyle}>
+        <div ref={resizableDraggableRef} className="resize-drag yes-drop" onClick={()=>handleClick(canvasObject)} style={itemStyle}>
             <div className="chart-item" ref={figureRef} id={`chart-container-${canvasObject.id}`}></div>
             <p className="highcharts-description"></p>
         </div>
--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./Text.css"
 
 function Text({canvasObject, onClick}){
+    const resizableDraggableRef = useRef()
     const itemStyle = {...canvasObject.size, ...canvasObject.position, transform: `scale(${canvasObject.scale})`}
 
+    function handleClick(canvasObject){
+        const resizableDraggableElements = document.querySelectorAll(".resize-drag")
+        resizableDraggableElements.forEach(element => {
+            element.classList.remove("active")
+        })
+
+        resizableDraggableRef.current.classList.add("active")
+        onClick(canvasObject)
+    }
+
     return (
-        <div className="resize-drag yes-drop" style={itemStyle} onClick={()=>onClick(canvasObject)}>
+        <div ref={resizableDraggableRef} className="resize-drag yes-drop" style={itemStyle} onClick={()=>handleClick(canvasObject)}>
             <p className="text-item" style={canvasObject.contentStyle}>{canvasObject.content}</p>
         </div>
     )
